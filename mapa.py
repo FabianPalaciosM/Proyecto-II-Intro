9f4884e10bc58111f39369d094f6608762d0f055
FabianPalaciosM <fabianpm29@gmail.com>
2025-11-27 11:09:16 -0600

    Reemplazo de matriz, creo que incorrecto

--- mapa.py
+++ mapa.py
@@ -3,15 +3,30 @@ import config
 from matriz import generar_mapa 
 
 
-mapa_numerico = generar_mapa(config.CANTIDAD_DE_FILAS, config.CANTIDAD_DE_COLUMNAS)
-
-def generar_matriz_clases():
-    global mapa_numerico
-
-    filas = len(mapa_numerico)
+def convertir_matriz_a_clases(matriz_numerica):
+    #E: La matriz numerica 
+    #S: La matriz con la clase respectiva en cada espacio
+    filas = len(matriz_numerica)
     columnas = len(filas)
 
-    matriz = []
     for i in range(filas):
         for j in range(columnas):
-            pass
+            if matriz_numerica[i][j] == 0:
+                matriz_numerica[i][j] = Camino(i, j)
+            elif matriz_numerica[i][j] == 1:
+                matriz_numerica[i][j] == Liana(i, j)
+            elif matriz_numerica[i][j] == 2:
+                matriz_numerica[i][j] == Tunel(i, j)
+            elif matriz_numerica[i][j] == 3:
+                matriz_numerica[i][j] == Muro(i, j)
+    matriz_numerica[filas-1][columnas-1] == Salida[filas-1][columnas-1]
+
+    return matriz_numerica
+
+
+
+def generar_mapa_completo():
+    matriz_numerica = generar_mapa(config.CANTIDAD_DE_FILAS, config.CANTIDAD_DE_COLUMNAS)
+    matriz_de_clases = convertir_matriz_a_clases(matriz_numerica)
+
+    return matriz_de_clases
